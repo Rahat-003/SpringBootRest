6ca3166d50c085e177628cd1f450cba158f2cd1f
rahat <rahatakhter4@gmail.com>
2025-03-03 15:51:07 +0600

    Learning AOP

---
+++ src/main/java/com/example/SpringBootRest/aop/LoggingAspect.java
@@ -0,0 +1,26 @@
+package com.example.SpringBootRest.aop;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    // return type, class-name.method-name, (args)
+
+    @Before("execution(* com.example.SpringBootRest.service.JobService.*(..))")
+    public void logMethodCall(JoinPoint jp) {
+        LOGGER.info("Method call: " + jp.getSignature().getName());
+    }
+
+
+}
--- src/main/java/com/example/SpringBootRest/service/JobService.java
+++ src/main/java/com/example/SpringBootRest/service/JobService.java
@@ -47,6 +47,6 @@ public class JobService {
     }
 
     public List<JobPost> search(String keyword) {
-        return repo.findByPostProfileContainingOrPostDescContaining(keyword, keyword);
+            return repo.findByPostProfileContainingOrPostDescContaining(keyword, keyword);
     }
 }
